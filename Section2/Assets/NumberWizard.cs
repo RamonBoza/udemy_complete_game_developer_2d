9286c99387ca3a14e87db1795bed3e41ab94a095
ramonboza <bozanayade@gmail.com>
2020-09-09 16:43:39 +0200

    Section2.2 - Introducing variables

--- Section2/Assets/NumberWizard.cs
+++ Section2/Assets/NumberWizard.cs
@@ -7,10 +7,14 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+            
+            
         Debug.Log("Welcome to number wizard, yo");
         Debug.Log("Pick a number");
-        Debug.Log("Highest number is: 1000");
-        Debug.Log("Lowest number is: 1");
+        Debug.Log("Highest number is: " + max);
+        Debug.Log("Lowest number is: " + min);
     }
 
     // Update is called once per frame
