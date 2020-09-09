49a5f2335750fe1ff989984995b02147bf968f68
ramonboza <bozanayade@gmail.com>
2020-09-09 17:19:39 +0200

    Section2.7 - Functions & encapsulation

--- Section2/Assets/NumberWizard.cs
+++ Section2/Assets/NumberWizard.cs
@@ -6,12 +6,21 @@ public class NumberWizard : MonoBehaviour
 {
     // Start is called before the first frame update
     
-    int max = 1000;
-    int min = 1;
-    int guess = 500;
+    int max;
+    int min;
+    int guess;
     
     void Start()
     {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+        
         Debug.Log("Welcome to number wizard, yo");
         Debug.Log("Pick a number");
         Debug.Log("Highest number is: " + max);
@@ -21,27 +30,30 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
         max = max + 1;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Up Arrow key was pressed.");
             min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Tell me if your number is higher or lower than: " + guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Down Arrow key was pressed.");
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Tell me if your number is higher or lower than: " + guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("I am a genious.");
+            StartGame();
         }
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Tell me if your number is higher or lower than: " + guess);
     }
 }
