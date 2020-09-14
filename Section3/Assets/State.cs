f98d117e8ea93c96d914572c7035eed212d4e93f
ramonboza <bozanayade@gmail.com>
2020-09-14 18:25:51 +0200

    Section3.7 - Creating An Array

--- Section3/Assets/State.cs
+++ Section3/Assets/State.cs
@@ -6,9 +6,15 @@ using UnityEngine;
 public class State : ScriptableObject
 {
     [TextArea(14,10)][SerializeField] private string storyText;
+    [SerializeField] private State[] nextStates;
 
     public string GetStateStory()
     {
         return storyText;
+    }
+
+    public State[] GetNextState()
+    {
+        return nextStates;
     }
 }
