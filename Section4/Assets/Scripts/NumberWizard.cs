ed4e899e9fd8f11edddc3e733c6bd4d090d075eb
ramonboza <bozanayade@gmail.com>
2020-09-16 01:24:59 +0200

    Section4.6 - Import and refactor code

---
+++ Section4/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+    
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = max + 1;
+        guess = (max - min ) / 2;
+    }
+    
+    // Update is called once per frame
+    public void OnPressHigher()
+    {
+            min = guess;
+            NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+            max = guess;
+            NextGuess();
+    }
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+    }
+}
