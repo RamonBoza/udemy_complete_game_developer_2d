eefccbc0cef23cbaf3d0f653e66bc90adc374209
Portatil <bozanayade@gmail.com>
2020-09-24 12:12:26 +0200

    Sectin5.6 - Distance as a Vector2

---
+++ Section5/Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //configuration
+    [SerializeField] private Paddle paddle;
+    
+    // state
+    private Vector2 distanceToBallVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distanceToBallVector = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePos = paddle.transform.position;
+        Vector2 ballPos = paddlePos + distanceToBallVector;
+        transform.position = ballPos;
+    }
+}
