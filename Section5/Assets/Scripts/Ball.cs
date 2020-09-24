6d355c23323b5d1fb095177f28001be566d51965
Portatil <bozanayade@gmail.com>
2020-09-24 12:21:09 +0200

    Section5.7 - Launch the ball

--- Section5/Assets/Scripts/Ball.cs
+++ Section5/Assets/Scripts/Ball.cs
@@ -6,9 +6,13 @@ public class Ball : MonoBehaviour
 {
     //configuration
     [SerializeField] private Paddle paddle;
+    [SerializeField] private float xPush = 2f;
+    [SerializeField] private float yPush = 15f;
     
     // state
     private Vector2 distanceToBallVector;
+
+    private bool hasStarted = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +21,25 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToThePaddle();
+            LaunchOnMouseClick();
+        }
+        
+    }
+
+    void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+        }
+    }
+
+    void LockBallToThePaddle()
     {
         Vector2 paddlePos = paddle.transform.position;
         Vector2 ballPos = paddlePos + distanceToBallVector;
