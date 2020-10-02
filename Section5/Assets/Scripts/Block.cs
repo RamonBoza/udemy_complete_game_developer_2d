d76680f27058a6cd793105917de024f93b094022
ramonboza <bozanayade@gmail.com>
2020-10-02 18:52:23 +0200

    Section5.15 - Count breakable blocks

--- Section5/Assets/Scripts/Block.cs
+++ Section5/Assets/Scripts/Block.cs
@@ -8,10 +8,19 @@ public class Block : MonoBehaviour
 {
 
     [SerializeField] private AudioClip breakSound;
-    
+
+    private Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        level.DestroyBreakableBlocks();
         Destroy(gameObject);
     }
 }
---
+++ Section5/Assets/Scripts/Level.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] private int breakableBlocks; //to debug this number
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void DestroyBreakableBlocks()
+    {
+        breakableBlocks--;
+    }
+}
