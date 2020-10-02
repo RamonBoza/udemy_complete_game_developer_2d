fdbfca8381513292d077ae4698bd6f0ce1d52322
ramonboza <bozanayade@gmail.com>
2020-10-02 18:42:16 +0200

    Section5.14 - PlayClipAtPoint

--- Section5/Assets/Scripts/Block.cs
+++ Section5/Assets/Scripts/Block.cs
@@ -2,11 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Block : MonoBehaviour
 {
+
+    [SerializeField] private AudioClip breakSound;
+    
     private void OnCollisionEnter2D(Collision2D other)
     {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
