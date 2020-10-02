43bb1dc9c995692fc3fc9db83bee265559d4939d
ramonboza <bozanayade@gmail.com>
2020-10-02 19:05:37 +0200

    Section5.16 - Win the level & load

--- Section5/Assets/Scripts/Level.cs
+++ Section5/Assets/Scripts/Level.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Level : MonoBehaviour
 {
     [SerializeField] private int breakableBlocks; //to debug this number
-
+    
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
@@ -14,5 +15,9 @@ public class Level : MonoBehaviour
     public void DestroyBreakableBlocks()
     {
         breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            // Load next scene        
+        }
     }
 }
