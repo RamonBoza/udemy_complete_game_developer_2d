527a5cc854a930336aabfbf03934c641d04278c3
ramonboza <bozanayade@gmail.com>
2020-10-02 19:13:39 +0200

    Section5.17 - Using Time.timeScale

---
+++ Section5/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    [Range(0.1f,10f)][SerializeField] private float gameSpeed = 1f;
+
+    private void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
