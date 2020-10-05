74f5654bf30937e5b2d6aa8dace5e5f2115baa8b
Portatil <bozanayade@gmail.com>
2020-10-06 00:18:31 +0200

    Section5.19 - Display player score

--- Section5/Assets/Scripts/GameStatus.cs
+++ Section5/Assets/Scripts/GameStatus.cs
@@ -2,15 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
+    //elements
+    [SerializeField] private TextMeshProUGUI scoreText;
     //config parameters
     [Range(0.1f,10f)][SerializeField] private float gameSpeed = 1f;
     [SerializeField] private int pointsPerBlockedDestroyed = 83;
     
     //state
     [SerializeField] private int currentScore = 0;
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString(); 
+    }
+
     private void Update()
     {
         Time.timeScale = gameSpeed;
@@ -19,5 +28,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlockedDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 }
