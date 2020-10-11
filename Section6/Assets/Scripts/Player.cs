58a85b3ea037c92bc0f68f38267ee1d6b786a574
Portatil <bozanayade@gmail.com>
2020-10-11 17:22:38 +0200

    Section6.3 - ViewPortToWorldPoint()

--- Section6/Assets/Scripts/Player.cs
+++ Section6/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.UIElements;
 using UnityEngine;
@@ -7,6 +8,13 @@ using UnityEngine.EventSystems;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float padding = 1f;
+
+    private float xMin, xMax, yMin, yMax;
+    private void Start()
+    {
+        SetUpMoveBoundaries();
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,12 +25,24 @@ public class Player : MonoBehaviour
     private void Move()
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
-
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newYPos = transform.position.y + deltaY;
         
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX,xMin,xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
         transform.position = new Vector2(newXPos, newYPos);
 
+    }
+
+    public void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        var lowerCorner = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        var topCorner = gameCamera.ViewportToWorldPoint(new Vector3(1, 1, 0));
+
+        xMin = lowerCorner.x + padding;
+        xMax = topCorner.x - padding;
+        yMin = lowerCorner.y + padding;
+        yMax = topCorner.y - padding;
     }
 }
