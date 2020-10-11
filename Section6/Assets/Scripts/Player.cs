76690bae6c4dac2c3876d048ca3d7218c12c4640
Portatil <bozanayade@gmail.com>
2020-10-11 17:11:36 +0200

    Section6.2 - Movement & Time.deltaTime

---
+++ Section6/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = transform.position.x + deltaX;
+
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY;
+        
+        transform.position = new Vector2(newXPos, newYPos);
+
+    }
+}
