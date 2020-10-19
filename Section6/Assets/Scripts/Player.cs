dd1205bc0f3dee2c4e4fbaa378513e443a8772d9
Portatil <bozanayade@gmail.com>
2020-10-19 12:52:27 +0200

    Section6.4 - Make player shoot

--- Section6/Assets/Scripts/Player.cs
+++ Section6/Assets/Scripts/Player.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
+    // parameters
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float padding = 1f;
-
+    [SerializeField] private GameObject laserPrefab;
+    [SerializeField] private float projectileSpeed;
+    
     private float xMin, xMax, yMin, yMax;
     private void Start()
     {
         SetUpMoveBoundaries();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
     }
 
     private void Move()
@@ -44,5 +48,14 @@ public class Player : MonoBehaviour
         xMax = topCorner.x - padding;
         yMin = lowerCorner.y + padding;
         yMax = topCorner.y - padding;
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var laser = Instantiate(laserPrefab, this.transform.position, Quaternion.identity);
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+        }
     }
 }
