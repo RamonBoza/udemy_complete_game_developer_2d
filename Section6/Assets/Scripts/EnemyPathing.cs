3f4560473b080bee952a424822ac35489d2779fc
Portatil <bozanayade@gmail.com>
2020-10-21 17:15:49 +0200

    Section6.8 - Move enemy on path

--- Section6/Assets/Scripts/EnemyPathing.cs
+++ Section6/Assets/Scripts/EnemyPathing.cs
@@ -1,8 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] private List<Transform> waypoints;
+    [SerializeField] private float moveSpeed = 2f;
+    private int waypointIndex = 0;
+
+    private void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position =
+                Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition) waypointIndex++;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
