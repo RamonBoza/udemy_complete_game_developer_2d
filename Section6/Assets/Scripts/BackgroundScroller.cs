39a596fc08d26e7f692df1ce7cac3c0db6337d79
Portatil <bozanayade@gmail.com>
2020-10-22 00:03:10 +0200

    Section6.18 - Scrolling background

---
+++ Section6/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] private float backgroundScrollSpeed = 0.5f;
+    private Material background;
+    private Vector2 offset;
+
+    private void Start()
+    {
+        background = GetComponent<Renderer>().material;
+        offset = new Vector2(0, backgroundScrollSpeed);
+    }
+
+    private void Update()
+    {
+        background.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
