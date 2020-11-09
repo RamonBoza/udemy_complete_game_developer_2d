04360e3505abf49f6e22c319d09853d21368489a
ramonboza <bozanayade@gmail.com>
2020-11-09 19:03:55 +0100

    Section7-9: Spawn attackers using coroutine

---
+++ Section7/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    private bool spawn = true;
+    [SerializeField] private float minSpawnDelay = 1f;
+    [SerializeField] private float maxSpawnDelay = 5f;
+    [SerializeField] private Attacker attackerPrefab;
+
+
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            Spawn();
+        }    
+    }
+
+    private void Spawn()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
