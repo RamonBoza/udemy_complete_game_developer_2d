50da52251c4e5eaed91f105ee8bb0bc038d3a7c8
Portatil <bozanayade@gmail.com>
2020-11-09 23:24:25 +0100

    Section7-10: Adding animation events

--- Section7/Assets/Scripts/Attacker.cs
+++ Section7/Assets/Scripts/Attacker.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
-    [Range(0f, 5f)][SerializeField] private float walkSpeed = 1f;
-    
-    void Start()
-    {
-        
-    }
-    
+    private float currentSpeed = 1f;
+
     void Update()
     {
-        transform.Translate(Vector2.left * (walkSpeed * Time.deltaTime));       
+        transform.Translate(Vector2.left * (currentSpeed * Time.deltaTime));       
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
     }
 }
