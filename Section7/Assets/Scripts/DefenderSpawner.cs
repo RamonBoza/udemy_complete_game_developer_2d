1624e7ab132e8e70e9ce9dec7bcc938ffb828ec0
ramonboza <bozanayade@gmail.com>
2020-11-10 18:02:34 +0100

    Section7-16: Click to spawn defenders

---
+++ Section7/Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject defender;
+
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        SpawnDefender();
+    }
+
+    private void SpawnDefender()
+    {
+        GameObject newDefender = Instantiate(defender, Vector2.zero, Quaternion.identity);
+    }
+}
