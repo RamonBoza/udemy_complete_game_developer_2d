24185ccd0ae5e1b2a975eba43a83295c04241d52
ramonboza <bozanayade@gmail.com>
2020-11-10 16:59:47 +0100

    Section7-14: Implementing health & damage

--- Section7/Assets/Scripts/Projectile.cs
+++ Section7/Assets/Scripts/Projectile.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float currentSpeed = 1f;
+    [SerializeField] private float damage = 50f;
 
     void Start()
     {
@@ -15,5 +17,11 @@ public class Projectile : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector2.right * (currentSpeed * Time.deltaTime));   
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var health = other.GetComponent<Health>();
+        health.DealDamage(damage);
     }
 }
