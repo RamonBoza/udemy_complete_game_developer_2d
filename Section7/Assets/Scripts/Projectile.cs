460c64b1ff08e89350e65d31195aaa4835676b5e
Portatil <bozanayade@gmail.com>
2020-11-10 10:30:56 +0100

    Section7-13: Make projectile move

---
+++ Section7/Assets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float currentSpeed = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * (currentSpeed * Time.deltaTime));   
+    }
+}
