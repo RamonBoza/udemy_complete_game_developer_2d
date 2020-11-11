5ffcc18ba3695fb1ae1c908d0c28cade4c11a0d0
ramonboza <bozanayade@gmail.com>
2020-11-11 15:06:53 +0100

    Section7-21: Start our resource system

---
+++ Section7/Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour
+{
+    [SerializeField] private int stars = 100;
+    private Text starText;
+    
+    void Start()
+    {
+        starText = GetComponent<Text>();
+    }
+
+    private void UpdateDisplays()
+    {
+        starText.text = stars.ToString();
+    }
+
+    public void AddStars(int amount)
+    {
+        stars += amount;
+        UpdateDisplays();
+    }
+
+    public void SpendStars(int amount)
+    {
+        if (stars >= amount)
+        {
+            stars -= amount;
+            UpdateDisplays();    
+        }
+    }
+}
